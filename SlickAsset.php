e3a026c377888cf04e208897f4223a4c7d65115d
Aleksandr Solovyov <solovyov.a.a.19@gmail.com>
2017-06-15 18:13:12 +0300

    Create asset

---
+++ SlickAsset.php
@@ -0,0 +1,20 @@
+<?php
+namespace kl83\slickasset;
+
+class SlickAsset extends \yii\web\AssetBundle
+{
+    public $sourcePath = '@bower/slick-carousel/slick';
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+    public $css = [
+        'slick.css',
+        'slick-theme.css',
+    ];
+    public function init()
+    {
+        $this->js = [
+            'slick'.( YII_DEBUG ? '.min' : '' ).'.js',
+        ];
+    }
+}
